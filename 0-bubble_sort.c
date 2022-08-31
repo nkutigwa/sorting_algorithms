c115c9293de809ee20e87d8ece157d0d4275aeab
nkutigwa <nkw802640@gmail.com>
2022-08-31 03:29:31 -0700

    updating task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,31 +1,40 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
- * bubble_sort - sorts an array with buble sort algorithm
- * @array: Array that going to be sorted
- * @size: Size of array
- * 
- * Return: nothing, just sort and print when swap.
+ * bubble_sort - sorts an array of integers in ascending order
+ * using the Bubble sort algorithm
+ * @array: array for to sort
+ * @size: number elements of array
+ * Return: sorts new array
  */
+
 void bubble_sort(int *array, size_t size)
 {
-	int tmp;
-	size_t i, j;
+	size_t i;
+	size_t j;
+	size_t flag;
+	size_t temp;
 
 	if (array == NULL || size == 0)
 		return;
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size - 1; j++)
+		flag = 0;
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				tmp = array[j];
+				temp = array[j];
 				array[j] = array[j + 1];
-				array[j + 1] = tmp;
+				array[j + 1] = temp;
+				flag = 1;
 				print_array(array, size);
 			}
+
 		}
+		if (flag == 0)
+			break;
 	}
 }
